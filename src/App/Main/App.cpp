6e73b0feee00acb546a06137e0e026c197784543
João Mathias Pereira Rezende <joao.mathias@audaces.com>
2017-12-14 11:21:46 -0200

    debugging start

--- src/App/Main/App.cpp
+++ src/App/Main/App.cpp
@@ -8,25 +8,53 @@ void App::start()
 void App::firstCommand()
 {
    int op;
-   int list;
 
    do {
       op= View::getFirstOption();
-      switch (op) {
+      switch (op) 
+      {
+      case 1: cmd= new cCreateBothLists();
+      case 2: cmd= new cDisplay();
+      case 3: cmd= new cConcatenate();
+      case 4: cmd= new cDeleteLists();
+      case 5: 
+      {
+         system("cls");
+         cmd= new cSetCurrentList();
+         secondCommand();
+         break;
       }
-   }
+      case 0: exit(0);
+      default: system("cls"); u->_InvalidOp(); break;
+      }
+   } while (op != NULL);
 }
 
 void App::secondCommand()
 {
+   int op;
+
+   do {
+      op= View::getSecondOption();
+      switch (op) 
+      {
+      case 1: cmd= new cPrepend();
+      case 2: cmd= new cAppend();
+      case 3: cmd= new cAddInPos();
+      case 4: cmd= new cDisable();
+      case 5: cmd= new cDisableAll();
+      case 6: cmd= new cDisplay();
+      case 7: cmd= new cDisplayDesc();
+      case 8: cmd= new cSort();
+      case 9: cmd= new cPurge();
+      case 10: system("cls"); op= NULL; break;
+      case 0: exit(0);
+      default: system("cls"); u->_InvalidOp();
+      }
+   } while (op != NULL);
 }
 
 void App::runCommand()
 {
-
-}
-
-void App::setCurrent()
-{
-
-}
+   cmd->exec(*m, *u);
+} 
